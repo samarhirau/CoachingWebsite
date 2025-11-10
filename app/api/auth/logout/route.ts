58e48f5357628dbae96c6e66a4a66be951c49373
samarhirau <samrhirau@gmail.com>
2025-11-10 13:20:34 +0530

    solve logut

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -3,7 +3,10 @@ import { NextResponse } from "next/server"
 export async function POST() {
   const response = NextResponse.json({ message: "Logged out successfully" })
 
-  response.cookies.delete("auth-token")
+   response.headers.set(
+    "Set-Cookie",
+    "auth-token=; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=0"
+  );
 
   return response
 }
